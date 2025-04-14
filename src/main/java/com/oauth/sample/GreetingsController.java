76b65e2f3ee9402199af3ff1b2cf0250f58c868d
박종범 <jbpark03@gmail.com>
2025-04-14 09:26:36 +0900

    check oauth2 shared info thru authentication

--- src/main/java/com/oauth/sample/GreetingsController.java
+++ src/main/java/com/oauth/sample/GreetingsController.java
@@ -1,5 +1,7 @@
 package com.oauth.sample;
 
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +10,7 @@ public class GreetingsController {
 
     @GetMapping("/hello")
     public String sayHello() {
+        var authen = SecurityContextHolder.getContext().getAuthentication();
         return "안녕?";
     }
 }

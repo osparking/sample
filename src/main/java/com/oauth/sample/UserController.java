2e64f6d6d5fb1f02e3ab5aaf3b96b61b297dbd3d
박종범 <jbpark03@gmail.com>
2025-04-15 08:23:43 +0900

    acquire user info and response w/ it

--- src/main/java/com/oauth/sample/UserController.java
+++ src/main/java/com/oauth/sample/UserController.java
@@ -1,5 +1,7 @@
 package com.oauth.sample;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +10,8 @@ import java.util.Map;
 @RestController
 public class UserController {
     @GetMapping("/user-info")
-    public Map<String, Object> userInfo() {
-        return null;
+    public Map<String, Object> userInfo(@AuthenticationPrincipal
+                                        OAuth2User principal) {
+        return principal.getAttributes();
     }
 }
